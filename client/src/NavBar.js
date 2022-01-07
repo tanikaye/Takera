8fa0100127975a9b5f6dc61164b196ebd524bfcd
Okera Johnson <okerajohnson@gmail.com>
2022-01-07 04:28:28 -0500

    Got those pesky words vamooshed

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import {Link} from "react-router-dom"
 
 function NavBar({ setUser, user }) {
+
+  const [shadow, setShadow] = useState(false);
+  
+
+   
 
   function handleLogoutClick() {
     console.log("logged out", user)
@@ -11,14 +16,16 @@ function NavBar({ setUser, user }) {
         .then(r=>r.json())
         .then(setUser(null));
 }
-
+    function weHidin(){
+        setShadow.setState({shadow:true})
+    }
     return (
         <nav className = "navBar">
             <Link className="hello" to="/ListingContainer">Items</Link>
             <Link className="hello" to="/About">About</Link>
             <Link className="hello" to="/PurContainer">Purchases</Link>
-            <Link className="hello" to="/Login">Login</Link>
-            <Link className="hello" to="/SignUp">SignUp</Link>
+            <Link className="hello" to="/Login">{  shadow ? "Login":"" }</Link>
+           <Link className="hello" to="/SignUp">{  shadow ? "SignUp":"" }</Link>
             <button onClick={handleLogoutClick}>
             <Link className="hello" to="/Logout" >Logout</Link>
             </button>
@@ -29,3 +36,4 @@ function NavBar({ setUser, user }) {
 }
 
 export default NavBar;
+
